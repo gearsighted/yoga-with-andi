3447987dc60d38ecef1e072b29a87438a241bdbf
Jesse Woody <gearsighted@gmail.com>
2021-08-24 17:52:43 -0700

    hero and header refactored, about section started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './index.css';
 import Hero from './components/Hero';
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
+      <About/>
     </div>
   );
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../index.css';
+
+function About () {
+    return (
+        <section className="flex justify-between">
+            <div id="about-text" className="w-full md:w-50% min">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio architecto quod fugiat explicabo tenetur recusandae quo temporibus. A voluptate consectetur nisi autem doloribus dicta perspiciatis rerum sint dolorem. Modi, ducimus.
+            Blanditiis incidunt excepturi officiis maiores impedit error deleniti, sit voluptates eaque facilis commodi alias! Esse velit aperiam cumque quibusdam maxime, impedit perferendis maiores placeat vitae eos animi. Libero, ad recusandae.
+            Necessitatibus commodi enim, numquam adipisci et blanditiis praesentium incidunt voluptatum quos cum! Veritatis minima fuga recusandae aspernatur ipsam minus. Voluptatum eveniet inventore tenetur rerum nemo ea nulla labore enim explicabo?
+            Saepe magni aliquam omnis? Blanditiis deserunt corrupti quae eveniet sit exercitationem cupiditate ipsa, quibusdam qui ipsum consequuntur eaque, nemo, fugiat magnam distinctio? Modi inventore labore ipsa ducimus nemo corrupti dolore!
+            Dolores dolore eius porro quos possimus et! Porro pariatur accusantium facilis laborum repudiandae nihil commodi dolore in voluptates eos aperiam ab dolorem, tempora ducimus suscipit inventore voluptatem, exercitationem fugit voluptas.</div>
+            <div id="about-image" className="w-full md:w-50% min"><img src="https://res.cloudinary.com/gearscloud/image/upload/q_auto:good/v1628440103/Yoga%20With%20Andi/andi-headshot_gvo3ql.webp"></img></div>
+
+        </section>
+    )
+}
+
+export default About;
