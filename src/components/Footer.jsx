41342b8830169a9e65b8ca86ceed43c6620bdb27
gearsighted <gearsighted@gmail.com>
2021-09-14 06:38:03 -0400

    removed all placeholder links to avoid ES-lint error

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import '../index.css';
 function Footer () {
     return (
         <div className="bg-gray-500 text-center flex justify-center px-6 py-12">
-            <a href="#" class="mx-6 font-medium hover:text-blue-400 focus:ring-blue-400">Charlottesville, VA</a>
-            <a href="#" class="mx-6 font-medium hover:text-blue-400 focus:ring-blue-400">senatroal@gmail.com</a>
+            <p class="mx-6 font-medium hover:text-blue-400 focus:ring-blue-400">Charlottesville, VA</p>
+            <p class="mx-6 font-medium hover:text-blue-400 focus:ring-blue-400">fakeemail@fake.fake</p>
         </div>
     )
 }
